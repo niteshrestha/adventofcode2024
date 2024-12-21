bb5f8e226f474bb9e1f185e6130c0f24309fc8a8
niteshrestha <4751915+niteshrestha@users.noreply.github.com>
2024-12-21 13:52:04 -0500

    Day 1: Part 2 Similarity Score

--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        if(args.Length > 0){
+        if(args.Length > 0)
+        {
             if(args[0] == "1"){
-                Day1.Execute();
+                Day1 day1 = new();
+                day1.GetTotalDistance();
+                day1.GetSimilarityScore();
             }
         }
     }
--- src/Puzzle/Day1.cs
+++ src/Puzzle/Day1.cs
@@ -1,38 +1,41 @@
 namespace adventofcode.Puzzle;
 
-public static class Day1{
-    public static void Execute(){
+public class Day1
+{
+    private readonly List<int> leftList = [];
+    private readonly List<int> rightList = [];
 
-        // Input puzzle
-        var firstList = new List<int>();
-        var secondList = new List<int>();
-
+    public Day1()
+    {
         // read from csv input and store in two lists
-
         string csvPath = "Puzzle/input/day1input.csv";
-        using(StreamReader reader = new StreamReader(csvPath))
+
+        using StreamReader reader = new(csvPath);
+        string? line;
+        while ((line = reader.ReadLine()) != null)
         {
-            string line;
-            while((line = reader.ReadLine()) != null){
-                string[] values = line.Split(',');
+            string[] values = line.Split(',');
 
-                // since we know the format of the csv file and know there will
-                // only be 2 values in the string array we will assign it.
-                firstList.Add(int.Parse(values[0])); 
-                secondList.Add(int.Parse(values[1])); 
-            }
+            // since we know the format of the csv file and know there will
+            // only be 2 values in the string array we will assign it.
+            leftList.Add(int.Parse(values[0]));
+            rightList.Add(int.Parse(values[1]));
         }
+    }
 
+    // Part 1
+    public void GetTotalDistance()
+    {
         int totalDistance = 0;
 
         // just sort the number to later substract to get the distance on each node
-        firstList.Sort();
-        secondList.Sort();
+        leftList.Sort();
+        rightList.Sort();
 
-        int length = firstList.Count;
+        int length = leftList.Count;
 
         // just making sure that I don't hit index out of bound
-        if(firstList.Count != secondList.Count)
+        if(leftList.Count != rightList.Count)
         {
             throw new InvalidOperationException("Length doesn't match");
         }
@@ -40,9 +43,31 @@ public static class Day1{
         // lets loop through each node in both list to get the distance
         // between them and add to get the total distance
         for(int i = 0; i < length; i++){
-            totalDistance += Math.Abs(firstList[i] - secondList[i]);
+            totalDistance += Math.Abs(leftList[i] - rightList[i]);
         }
-        
+
         Console.WriteLine($"Total Distance: {totalDistance}");
+    }
+
+    // Part 2
+    public void GetSimilarityScore()
+    {
+        int similarityScore = 0;
+
+        // find the occurence of numbers if the right list
+        var rightListOccurence = rightList.GroupBy(x=>x)
+            .Select(n => new { Element = n.Key, Count = n.Count()});
+
+        foreach(var leftListItem in leftList)
+        {
+            var occurence = rightListOccurence
+                .Where(x => x.Element == leftListItem)
+                .Select(x=>x.Count)
+                .FirstOrDefault();
+
+            similarityScore += leftListItem * occurence;
+        }
+
+        Console.WriteLine($"Similarity Score: {similarityScore}");
     }
 }
